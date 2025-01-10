59dd7d1c09662ac183687bf421a828be489b6fd8
PyDevC <anuskcse@gmail.com>
2025-01-10 10:03:54 -0500

    can now dry run files in scripts and also run them

--- run.sh
+++ run.sh
@@ -2,20 +2,39 @@
 
 scripts_dir="$(cd "$(dirname ${BASH_SOURCE[0]} &> /dev/null && pwd)")"
 
-echo $scripts_dir
 dry="0"
 grep=""
-echo $#
 
 #check for arguments
 while [[ $# -gt 0 ]]; do
   if [[ "$1" == "--dry" ]]; then
-    $dry="1"
-    echo "log"
+    dry="1"
   elif [[ "$1" == "" ]]; then 
-    echo "exe"
+    grep="$1"
   else
-    echo "no argument";
+    echo "behavior undefined";
+  fi
+  shift
+done
+
+log(){
+  echo "${1##*/}"
+  while read -r line;do 
+    if [ "$line" != "${line#*#}" ]; then    
+      printf '%s\n' "$line"
+    fi
+  done < "$1" 
+}
+  
+
+scripts_dir="$PWD/scripts"
+
+for file in "$scripts_dir"/*; do
+  if [[ "$dry" == "1" ]]; then
+    printf "[[ DRY RUN:]]\n"
+    log "$file"
+  else 
+    $file
   fi
 done
 

805255b1299c51e785a87bf068533c11ab04c6c6
PyDevC <anuskcse@gmail.com>
2025-02-25 09:16:15 -0500

    fixed config issue

--- configure.sh
+++ configure.sh
@@ -5,3 +5,11 @@ cp -r !(tmux.conf) $HOME
 
 mkdir $XDG_CONFIG_HOME/tmux/
 cp tmux.conf $XDG_CONFIG_HOME/tmux
+
+olduser=("name = Username")
+oldemail=("email = email")
+
+newuser=("name = $GithubUsername")
+newemail=("email = $GithubEmail")
+
+sed -i -e 's/$(old)/$(new)/g' ~/.gitconfig

e00a7f7e9c9f042cb363716807f6fa2022a944d2
PyDevC <anuskcse@gmail.com>
2025-03-14 20:31:40 +0530

    added libglew-dev install

--- runs/opengl.sh
+++ runs/opengl.sh
@@ -4,7 +4,7 @@ if [[ ! -d $HOME/personal/github/freeglut ]];then
     git clone https://github.com/freeglut/freeglut.git ~/personal/github/freeglut
 fi
 
-sudo apt-get install libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev libglut-dev libglut3.12 libice-dev libsm-dev libxt-dev
+sudo apt-get install libglfw3-dev libgl2-mesa-dev libglu1-mesa-dev libglut-dev libglut3.12 libice-dev libsm-dev libxt-dev libglew-dev
 
 cd ~/personal/github/freeglut
 

2c3dbc16ecee80bbce31f7f7de58553673476686
PyDevC <anuskcse@gmail.com>
2025-05-02 22:20:30 +0530

    update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # BUILD Desktop
 
 some are copied from primeagen and some are by me
+
+## List of other softwares:
+- OBS
--- runs/neovim.sh
+++ runs/neovim.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-version="v0.10.2"
+version="master"
 if [[ ! -z $1 ]];then
     version=$1
 fi
 
+neovim_loc=$HOME/personal/neovim
+
 sudo apt -y install cmake gettext lua5.1 liblua5.1-0-dev
 
-if [[ ! -d $HOME/neovim ]];then
-    git clone https://github.com/neovim/neovim.git $HOME
+if [[ ! -d $neovim_loc ]];then
+    git clone https://github.com/neovim/neovim.git $neovim_loc
 fi
 
 # fetch latest updates
-git -C ~/neovim fetch --all
-git -C ~/neovim checkout $version
+git -C $neovim_loc fetch --all
+git -C $neovim_loc checkout $version
 
-make -C ~/neovim clean
-make -C ~/neovim CMAKE_BUILD_TYPE=RelWithDebInfo
-sudo make -C ~/neovim install
+make -C $neovim_loc clean
+make -C $neovim_loc CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make -C $neovim_loc/build install

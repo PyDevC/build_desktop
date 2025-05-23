369aedd5e3dcfe5c8f5d7816db52489c35d420bc
PyDevC <anuskcse@gmail.com>
2025-05-23 23:15:17 +0530

    install hyprland

---
+++ runs/hyprland.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo dnf install -y ttf-font-awesome waybar ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
+sudo dnf install -y hyprlock
+sudo dnf install -y hyprpaper hyprshot
+sudo dnf install -y wlogout-git

7044b808f5afc1b9acf7611308807384d2991907
PyDevC <anuskcse@gmail.com>
2025-05-23 22:22:53 +0530

    upgraded the docs

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # BUILD Desktop
 
 Get up ready running ubuntu desktop
+
+```bash
+export PACK_MANG=dnf # for fedora
+export PACK_MANG=apt # for ubuntu
+```
--- runs/zsh.sh
+++ runs/zsh.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt install zsh
+sudo $PACK_MANG install zsh
 hash -r
 sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"

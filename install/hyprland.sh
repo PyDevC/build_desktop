d7ec7a8c62f2d420fe667ac2fd807db71694fc4e
PyDevC <anuskcse@gmail.com>
2025-07-08 18:13:34 +0530

    completed all installation scripts and created firstrun script

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+
 # Fedora
+
+sudo dnf copr enable solopasha/hyprland
 sudo dnf install -y ttf-font-awesome waybar ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
 sudo dnf install -y hyprlock
 sudo dnf install -y hyprpaper hyprshot
--- install/neovim.sh
+++ install/neovim.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-# Ubuntu
-sudo apt install -y cmake gettext lua5.1 liblua5.1-0-dev
-../runs/neovim.sh
 
-# Fedora
-sudo dnf install -y cmake gettext lua
-../runs/neovim.sh
+if [[ "$1" == "ubuntu-x" || "$1" == "ubuntu-w" ]]; then
+    sudo apt install -y cmake gettext lua5.1 liblua5.1-0-dev luarocks npm
+    ../runs/neovim.sh
+elif [[ "$1" == "fedora-x" || "$1" == "Fedora-w" ]]; then
+    sudo dnf install -y cmake gettext lua lua5.1 luarocks npm
+    ../runs/neovim.sh
+fi

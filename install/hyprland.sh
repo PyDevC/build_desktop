61a1e4f60bc467dfca701db54859742c081b4a03
PyDevC <anuskcse@gmail.com>
2025-07-11 20:17:36 +0530

    can't install fonts this way

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -3,7 +3,6 @@
 # Fedora
 
 sudo dnf copr enable solopasha/hyprland
-sudo dnf install -y ttf-font-awesome waybar ttf-jetbrains-mono-nerd ttf-nerd-fonts-symbols
 sudo dnf install -y hyprlock
 sudo dnf install -y hyprpaper hyprshot
 sudo dnf install -y wlogout-git

43ed72a38ef3a9ff8714cf51d103a1d789cc09ab
PyDevC <anuskcse@gmail.com>
2025-07-21 08:01:29 -0400

    Added fast syntax highlighthing for ohmyzsh

--- install/zsh.sh
+++ install/zsh.sh
@@ -4,3 +4,5 @@ sudo dnf install -y zsh
 hash -r
 sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting

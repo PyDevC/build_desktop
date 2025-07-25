7f518b206a707d6cca689d3b570192e7ba6b31bb
PyDevC <anuskcse@gmail.com>
2025-07-25 14:32:03 -0400

    Complete ROCm installation for Ubuntu

--- runs/rocm.sh
+++ runs/rocm.sh
@@ -1,29 +1,14 @@
 #!/usr/bin/env bash
 
-echo "WARNING: Not completed do not run"
-while [ : ]; do
-    read -p "Still want to run [y/N]: " run
-    echo $run
-    if [[ $run == "n" || $run == "N" ]];then
-        exit 0 
-    elif [[ $run == "y" || $run == "Y" ]]; then
-        echo "Running: "
-        break
-    else
-        echo "Choose either y or N"
-    fi
-done
-
-
 log(){
-    if [[ $1 == "-i" ]]; then
+    if [[ $1 == "-i" || $# -eq 1 ]]; then
         echo "Installing: $2"
     elif [[ $1 == "-p" ]]; then
         echo "Checking permissions and membership: $2"
     elif [[ $1 == "-n" ]]; then
         echo "$2"
-    else 
-        echo "Installing: $1"
+    else
+        echo "$@"
     fi
     echo "\n"
 }
@@ -40,7 +25,7 @@ log "-p" "Group Membership"
 sudo usermod -a -G video,render $LOGNAME
 # For customizing group permissions go through ROCm official installation prerequisites and modify above line
 
-log "Signing key: Can be changed as fedora doesn't use apt"
+log "Package signing key" " only for Ubuntu"
 # Make the directory if it doesn't exist yet.
 # This location is recommended by the distribution maintainers.
 sudo mkdir --parents --mode=0755 /etc/apt/keyrings
@@ -53,8 +38,6 @@ wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
 # Need custom installation for this
 
 log "Reginstering packages"
-# Need to change this 
-# This one is difficult to change as the installation packages are not clear also we can build from the source if needed but need more clear guidance for that
 
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.4.1 noble main" \
     | sudo tee --append /etc/apt/sources.list.d/rocm.list
@@ -74,6 +57,12 @@ sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
 EOF
 sudo ldconfig
 
-log "-n" "Configure ROCm path"
-export PATH=$PATH:/opt/rocm-6.4.1/bin # THis is more sensible
+log "-n" "Export ROCm path"
+export PATH=$PATH:/opt/rocm-6.4.1/bin
 export LD_LIBRARY_PATH=/opt/rocm-6.4.1/lib
+
+echo "Add this to your .zshrc or .bashrc"
+echo "export PATH=$PATH:/opt/rocm-6.4.1/bin"
+echo "export LD_LIBRARY_PATH=/opt/rocm-6.4.1/lib"
+
+echo "-n" "Installation complete reboot your system"

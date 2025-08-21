e7baf0f1a5587069efe73fb04f42310cc54b099b
PyDevC <anuskcse@gmail.com>
2025-08-21 09:06:18 -0400

    Completed all TODOs

--- install/ghostty.sh
+++ install/ghostty.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo snap install ghostty 
+sudo snap install ghostty --classic
 # I am against using snap package but this is the only thing available other than using build version
